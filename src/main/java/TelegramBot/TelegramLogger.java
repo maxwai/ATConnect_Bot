00f3a8bed27fc1af10a76c6dfc9d4722a4efcdfe
MaxWai <44324946+maxwai@users.noreply.github.com>
2021-01-28 16:44:28 +0100

    changed that in the logs \n get displayed as character

--- src/main/java/TelegramBot/TelegramLogger.java
+++ src/main/java/TelegramBot/TelegramLogger.java
@@ -22,27 +22,36 @@ public class TelegramLogger {
 		return name;
 	}
 	
+	public static String convertNewLine(String message) {
+		return message.replace("\n", " \\n ");
+	}
+	
 	public void trace(String message) {
+		message = convertNewLine(message);
 		logger.trace(message);
 		TelegramBots.sendLog(name, "Trace", message);
 	}
 	
 	public void debug(String message) {
+		message = convertNewLine(message);
 		logger.debug(message);
 		TelegramBots.sendLog(name, "Debug", message);
 	}
 	
 	public void info(String message) {
+		message = convertNewLine(message);
 		logger.info(message);
 		TelegramBots.sendLog(name, "Info", message);
 	}
 	
 	public void warn(String message) {
+		message = convertNewLine(message);
 		logger.warn(message);
 		TelegramBots.sendImportantLog(name, "Warning", message);
 	}
 	
 	public void error(String message) {
+		message = convertNewLine(message);
 		logger.error(message);
 		TelegramBots.sendImportantLog(name, "Error", message);
 	}
